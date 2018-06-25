61d77c6201c86faf366fa877548c62ca80faf991
Dmitriy Dementor <dmitriydementor@gmail.com>
2018-06-25 23:42:37 +0300

    Added basic chat functionality, personal chats, etc.

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '@pages/home/home';
+import { ChatListPage } from '@pages/chat-list/chat-list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { WebsocketProvider } from '@providers/websocket/websocket';
+import { ChatProvider } from '@providers/chat/chat';
+import { UserProvider } from '@providers/user/user';
+
+@NgModule({
+    declarations: [
+        MyApp,
+        HomePage,
+        ChatListPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ChatListPage,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler },
+        WebsocketProvider,
+        ChatProvider,
+        UserProvider,
+    ]
+})
+export class AppModule { }
